0d40e6541734eebc08213966540b26ac84c7c564
Karin Huber <karin@software-architects.at>
2025-01-12 10:06:42 +0100

    Update coming soon overlay

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,10 @@
 
 export default new Map([
-["src/content/pages/impressum.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fimpressum.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/dr-peter-huber.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fdr-peter-huber.mdx&astroContentModuleFlag=true")],
-["src/content/pages/leistungen.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fleistungen.mdx&astroContentModuleFlag=true")],
-["src/content/pages/wahlarzt.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fwahlarzt.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/home.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fhome.mdx&astroContentModuleFlag=true")],
+["src/content/pages/impressum.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fimpressum.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/kontakt.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fkontakt.mdx&astroContentModuleFlag=true")],
-["src/content/pages/ordination.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fordination.mdx&astroContentModuleFlag=true")]]);
+["src/content/pages/wahlarzt.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fwahlarzt.mdx&astroContentModuleFlag=true")],
+["src/content/pages/ordination.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fordination.mdx&astroContentModuleFlag=true")],
+["src/content/pages/leistungen.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fleistungen.mdx&astroContentModuleFlag=true")]]);
 		
