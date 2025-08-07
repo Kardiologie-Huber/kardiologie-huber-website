c99fe43624407aae3450f6ffca5f46d22aebd89c
Karin Huber <karin@software-architects.at>
2025-08-07 15:53:48 +0200

    Remove unfinished content

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const EMAIL = 'info@kardiologie-huber.at';
-export const PHONE = '+43 664 123 45 67';
+export const PHONE = '0681 813 612 95';
 export const WEB = 'https://kardiologie-huber.at';
