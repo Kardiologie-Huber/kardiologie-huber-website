ffded3ede206517e8095c10c3270b47dc8dacd14
Karin Huber <karin@software-architects.at>
2025-08-10 09:33:55 +0200

    favicon, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.kardiologie-huber.at',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   buildOptions: {
     site: 'https://www.kardiologie-huber.at',
   },
