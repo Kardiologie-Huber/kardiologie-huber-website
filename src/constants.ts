c854e93e3a4549e8cb61721d9b82c6c4c0951569
Karin Huber <karin@software-architects.at>
2025-08-14 06:38:47 +0200

    Book appointment link

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const EMAIL = 'ordination@kardiologie-huber.at';
 export const PHONE = '0681 813 612 95';
 export const WEB = 'https://kardiologie-huber.at';
+export const LATIDO = 'https://patient.latido.at/arzt/67540dd8be63d3480172fbae';
